2ce03b46a7615b115d1baa4f1096dade4f6e8e0f
petervil <pedro.borba.vilela@gmail.com>
2026-08-31 21:32:21 -0300

    aula 3108

--- src/main/java/br/edu/ifpe/oxefood/util/EntidadeNegocio.java
+++ src/main/java/br/edu/ifpe/oxefood/util/EntidadeNegocio.java
@@ -1,5 +1,12 @@
 package br.edu.ifpe.oxefood.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +14,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = { "id" })
+@MappedSuperclass
 public abstract class EntidadeNegocio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @JsonIgnore
+    @Column
     private Boolean habilitado;
-    
+
 }
